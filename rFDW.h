50d1b4bb289c7ccd080092f9b6931606d0959e97
ms234ms234 <ms234@suremail.info>
2014-11-27 11:38:13 +0600

    Update rFDW.h
    Compiler doesn't like defines that look like functions. Now they look different.

--- rFDW.h
+++ rFDW.h
@@ -24,51 +24,51 @@
 //     http://academy.kaziunas.com/tutorials/images/embedded_programming/Atmega168Pinout_Arduino.png
 
 // digitalWrite(myPin, LOW)
-#define		rFDW( 0,0)		PORTD &= 0b11111110
-#define		rFDW( 1,0)		PORTD &= 0b11111101
-#define		rFDW( 2,0)		PORTD &= 0b11111011
-#define		rFDW( 3,0)		PORTD &= 0b11110111
-#define		rFDW( 4,0)		PORTD &= 0b11101111
-#define		rFDW( 5,0)		PORTD &= 0b11011111
-#define		rFDW( 6,0)		PORTD &= 0b10111111
-#define		rFDW( 7,0)		PORTD &= 0b01111111
+#define		rFDW_0_0		PORTD &= 0b11111110
+#define		rFDW_1_0		PORTD &= 0b11111101
+#define		rFDW_2_0		PORTD &= 0b11111011
+#define		rFDW_3_0		PORTD &= 0b11110111
+#define		rFDW_4_0		PORTD &= 0b11101111
+#define		rFDW_5_0		PORTD &= 0b11011111
+#define		rFDW_6_0		PORTD &= 0b10111111
+#define		rFDW_7_0		PORTD &= 0b01111111
 
-#define		rFDW( 8,0)		PORTB &= 0b11111110
-#define		rFDW( 9,0)		PORTB &= 0b11111101
-#define		rFDW(10,0)		PORTB &= 0b11111011
-#define		rFDW(11,0)		PORTB &= 0b11110111
-#define		rFDW(12,0)		PORTB &= 0b11101111
-#define		rFDW(13,0)		PORTB &= 0b11011111
+#define		rFDW_8_0		PORTB &= 0b11111110
+#define		rFDW_9_0		PORTB &= 0b11111101
+#define		rFDW_10_0		PORTB &= 0b11111011
+#define		rFDW_11_0		PORTB &= 0b11110111
+#define		rFDW_12_0		PORTB &= 0b11101111
+#define		rFDW_13_0		PORTB &= 0b11011111
 
-#define		rFDW(14,0)		PORTC &= 0b11111110
-#define		rFDW(15,0)		PORTC &= 0b11111101
-#define		rFDW(16,0)		PORTC &= 0b11111011
-#define		rFDW(17,0)		PORTC &= 0b11110111
-#define		rFDW(18,0)		PORTC &= 0b11101111
-#define		rFDW(19,0)		PORTC &= 0b11011111
+#define		rFDW_14_0		PORTC &= 0b11111110
+#define		rFDW_15_0		PORTC &= 0b11111101
+#define		rFDW_16_0		PORTC &= 0b11111011
+#define		rFDW_17_0		PORTC &= 0b11110111
+#define		rFDW_18_0		PORTC &= 0b11101111
+#define		rFDW_19_0		PORTC &= 0b11011111
 
 // digitalWrite(myPin, HIGH)
-#define		rFDW( 0,1)		PORTD |= 0b00000001
-#define		rFDW( 1,1)		PORTD |= 0b00000010
-#define		rFDW( 2,1)		PORTD |= 0b00000100
-#define		rFDW( 3,1)		PORTD |= 0b00001000
-#define		rFDW( 4,1)		PORTD |= 0b00010000
-#define		rFDW( 5,1)		PORTD |= 0b00100000
-#define		rFDW( 6,1)		PORTD |= 0b01000000
-#define		rFDW( 7,1)		PORTD |= 0b10000000
+#define		rFDW_0_1		PORTD |= 0b00000001
+#define		rFDW_1_1		PORTD |= 0b00000010
+#define		rFDW_2_1		PORTD |= 0b00000100
+#define		rFDW_3_1		PORTD |= 0b00001000
+#define		rFDW_4_1		PORTD |= 0b00010000
+#define		rFDW_5_1		PORTD |= 0b00100000
+#define		rFDW_6_1		PORTD |= 0b01000000
+#define		rFDW_7_1		PORTD |= 0b10000000
 
-#define		rFDW( 8,1)		PORTB |= 0b00000001
-#define		rFDW( 9,1)		PORTB |= 0b00000010
-#define		rFDW(10,1)		PORTB |= 0b00000100
-#define		rFDW(11,1)		PORTB |= 0b00001000
-#define		rFDW(12,1)		PORTB |= 0b00010000
-#define		rFDW(13,1)		PORTB |= 0b00100000
+#define		rFDW_8_1		PORTB |= 0b00000001
+#define		rFDW_9_1		PORTB |= 0b00000010
+#define		rFDW_10_1		PORTB |= 0b00000100
+#define		rFDW_11_1		PORTB |= 0b00001000
+#define		rFDW_12_1		PORTB |= 0b00010000
+#define		rFDW_13_1		PORTB |= 0b00100000
 
-#define		rFDW(14,1)		PORTC |= 0b00000001
-#define		rFDW(15,1)		PORTC |= 0b00000010
-#define		rFDW(16,1)		PORTC |= 0b00000100
-#define		rFDW(17,1)		PORTC |= 0b00001000
-#define		rFDW(18,1)		PORTC |= 0b00010000
-#define		rFDW(19,1)		PORTC |= 0b00100000
+#define		rFDW_14_1		PORTC |= 0b00000001
+#define		rFDW_15_1		PORTC |= 0b00000010
+#define		rFDW_16_1		PORTC |= 0b00000100
+#define		rFDW_17_1		PORTC |= 0b00001000
+#define		rFDW_18_1		PORTC |= 0b00010000
+#define		rFDW_19_1		PORTC |= 0b00100000
 
 // Thanks for your attention.
